8bd3973b567241a9d0e2cd203fa1632de4f9693f
Henry Ball <henry_ball@hotmail.co.uk>
2018-11-29 17:10:09 +0000

    second commit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import the Router component
 import history from "./history";
 import { Router } from "react-router-dom";
 
-// import the App component
 import App from "./App";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
 import initial from "./data/initial";
 import reducer from "./data/reducer";
 
-// create some dummy articles
-// store them in an object
-// so that we can easily reference each one by its id
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initial, composeEnhancers(applyMiddleware(thunk)));
 
-const store = createStore(
-    reducer,
-    initial,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-
-
-// wrap the app in Router
-// pass our articles through to App using props
 ReactDOM.render(
     <Provider store={ store }>
         <Router history={ history }>
